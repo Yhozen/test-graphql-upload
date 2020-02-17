05cf7be3e38470460775c14c86d74712abfb2770
Gabriel Pérez <gabriel@garox.org>
2020-02-17 14:05:25 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import gql from 'graphql-tag'
-import { useMutation, useQuery } from '@apollo/react-hooks'
-import { Voyager } from 'graphql-voyager'
+import { useMutation } from '@apollo/react-hooks'
 
-import axios from 'axios'
 const MUTATION = gql`
-  mutation uploadFile($file: Upload!) {
-    uploadFile(file: $file) {
+  mutation uploadFile($input: Upload!) {
+    uploadFile(input: $input) {
       url
       success
     }
   }
 `
-const introspectionProvider = async query => {
-  return fetch('http://localhost:3001/graphql', {
-    method: 'post',
-    mode: 'no-cors',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: query })
-  }).then(response => response.json())
-}
-
-const api = axios.create({
-  baseURL: 'https://api-uat.kushkipagos.com',
-  headers: {
-    'Private-Merchant-Id': 20000000106532140000
-  },
-  timeout: 5000
-})
 
 function App() {
   const [mutate] = useMutation(MUTATION)
@@ -43,19 +25,13 @@ function App() {
     validity.valid && mutate({ variables: { file } })
   }
 
-  const form = useRef()
-
   return (
     <div className="App">
       <header className="App-header">
-        <Voyager introspection={introspectionProvider} />
         <img src={logo} className="App-logo" alt="logo" />
-        <form ref={form} id="kushki-pay-form" action="confirm" method="post">
-          <input type="hidden" name="cart_id" value="123" />
-        </form>
         <input type="file" required onChange={onChange} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.ss
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
